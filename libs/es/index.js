7d19a9485ea295ab247d17e507d49bc8622f9f7a
json chou <jsonchou329@gmail.com>
2020-01-15 16:17:03 +0800

    chore(release): v2.0.5 remove file module

--- libs/es/index.js
+++ libs/es/index.js
@@ -5,12 +5,11 @@
  */
 import zaxString from './string';
 import zaxFunc from './func';
-import zaxFiles from './files';
 import zaxArray from './array';
 import zaxObject from './object';
 import zaxRegex from './regex';
 import zaxRegexForm from './regexForm';
 import zaxTypes from './types';
 import zaxCases from './cases';
-export { zaxString, zaxFunc, zaxFiles, zaxArray, zaxObject, zaxRegex, zaxRegexForm, zaxTypes, zaxCases };
+export { zaxString, zaxFunc, zaxArray, zaxObject, zaxRegex, zaxRegexForm, zaxTypes, zaxCases };
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,6 @@
 
 import zaxString from './string'
 import zaxFunc from './func'
-import zaxFiles from './files'
 import zaxArray from './array'
 import zaxObject from './object'
 import zaxRegex from './regex'
@@ -16,5 +15,5 @@ import zaxCases from './cases'
 
 type Nothing = {} // jsdoc2md bugs, do not remove this line
 
-export { zaxString, zaxFunc, zaxFiles, zaxArray, zaxObject, zaxRegex, zaxRegexForm, zaxTypes, zaxCases }
+export { zaxString, zaxFunc, zaxArray, zaxObject, zaxRegex, zaxRegexForm, zaxTypes, zaxCases }
 
